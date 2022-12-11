5543a2a6012627e85497f62875824d5bb594fb06
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2022-12-11 11:00:11 -0500

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -177,7 +177,7 @@ const Hero = (props) => {
              style={{ marginTop: '10px' }}
             >
              <ViewInArIcon></ViewInArIcon>
-             View in AR Testing
+             View in AR Testing 45 new changes
             </Button>
            </div>
           </div>
