ac926500caa856cd24c3874918f271b27a483a1f
Dawar Mustaqeem <45918546+dmustaqeem@users.noreply.github.com>
2022-12-17 02:37:09 +0500

    fixed link error

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -171,13 +171,13 @@ const Hero = (props) => {
               })
 
               var win = window.open(
-               'https://food2viewardemo.netlify.app/',
+               'https://ar.food2view.com/ '+ '?Model=' +  ModelName + '/' + Resturant,
                '_blank'
               )
               win.focus()
-                   }}
+              }}
 
-
+                   
              variant="contained"
              style={{ marginTop: '10px' }}
             >
